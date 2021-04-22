d6a39dd960a22ebf7b5a2464c36f04cdc8e3fe00
mana0517 <80468194+mana0517@users.noreply.github.com>
2021-04-23 00:27:41 +0530

    Add files via upload

---
+++ array element.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5],i;
+    cout << arr[i];
+    cout << "Enter elements: ";
+    for (int i = 0; i < 5;++i)
+    cin >>arr[i];
+    cout << "You entered: ";
+    for (int i = 0; i < 5;++i)
+    cout << endl << *(arr+i);
+    return 0;
+}
+
